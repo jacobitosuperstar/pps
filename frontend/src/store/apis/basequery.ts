5ede1d556513afe07afa1fdf86cec90b1541f086
axelvalles <agabrielv1799@gmail.com>
2023-11-28 21:31:41 -0500

    feat: agregar cookie al request

--- frontend/src/store/apis/basequery.ts
+++ frontend/src/store/apis/basequery.ts
@@ -1,5 +1,18 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import Cookies from "js-cookie";
 
 export const appBaseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:8000/",
+  // credentials: "include",
+  prepareHeaders: (headers, context) => {
+    if (context.endpoint === "pin") return headers;
+
+    // Retrieve the CSRF token from the cookie
+    const csrfToken = Cookies.get("csrftoken");
+    // Set the CSRF token in the headers
+    headers.set("X-CSRFToken", csrfToken || "");
+    headers.set("Content-Type", "application/json");
+
+    return headers;
+  },
 });
