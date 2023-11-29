94c686a25a4955cc77c971b23041f6e0b61cf31d
Jacobo Mateo Bedoya Oquendo <jacobomateobedoya@icloud.com>
2023-11-28 21:26:46 -0500

    FIX: Sending an empty HTML response.

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,13 +1,35 @@
-from django.http import JsonResponse
-from django.http import HttpRequest
+from django.http import (
+    HttpRequest,
+    HttpResponse,
+    JsonResponse,
+)
 from django.views.decorators.http import require_GET
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.contrib.auth.decorators import login_required
 from django.utils.timezone import now
 
 
-@ensure_csrf_cookie
+
 @require_GET
+def new_pin_view(request: HttpRequest) -> HttpResponse:
+    """Server pining. To check that the server is alive and getting the csrf
+    cookie.
+
+    Parameters
+    ----------
+    request: HttpRequest
+        - GET
+
+    Returns
+    -------
+    JsonResponse
+        Json Object with the current time.
+    """
+    return HttpResponse(b"")
+
+
+@require_GET
+@ensure_csrf_cookie
 def pin_view(request: HttpRequest) -> JsonResponse:
     """Server pining. To check that the server is alive and getting the csrf
     cookie.
