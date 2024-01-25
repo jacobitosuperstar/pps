aedadcd022d16b7794c608eb326c88685f0177d7
axelvalles <agabrielv1799@gmail.com>
2024-01-25 10:36:00 -0500

    feat: crear componente cell table message
    el componente sirve para agregar un mensaje y un icono

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as AppDrawer } from "./app-drawer";
 export { default as AppNavbar } from "./app-navbar";
 export { default as AuthenticationValidator } from "./authentication-validator";
+export { default as CellTableMessage } from "./cell-table-message";
